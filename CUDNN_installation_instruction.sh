2555f213597d0e2c5ee33c3dc42b3a98c459648f
Chanapai Chuadchum <kornbot359@hotmail.com>
2024-01-20 14:23:51 +0700

    Update CUDNN_installation_instruction.sh

--- CUDNN_installation_instruction.sh
+++ CUDNN_installation_instruction.sh
@@ -3,16 +3,16 @@ curl -O https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 sh Miniconda3-latest-Linux-x86_64.sh
 sudo ubuntu-drivers devices
 sudo ubuntu-drivers autoinstall
-sudo apt install nvidia-driver-455 -y 
+sudo apt install nvidia-driver-535 -y 
 sudo apt install nvidia-cuda-toolkit -y 
 nvcc -V
 whereis cuda
-sudo cp /media/$USER/C8B063EAB063DD86/cudartinstaller.sh -t /home/$USER/
-./cudartinstaller.sh
-sudo cp /media/$USER/C8B063EAB063DD86/cudnn-10.1-linux-x64-v7.6.5.32.tgz -t /home/$USER
-tar -xvzf cudnn-10.1-linux-x64-v7.6.5.32.tgz
-sudo cp /media/$USER/C8B063EAB063DD86/fixerror.sh -t /home/$USER
-./fixerror.sh
+#sudo cp /media/$USER/C8B063EAB063DD86/cudartinstaller.sh -t /home/$USER/
+#./cudartinstaller.sh
+#sudo cp /media/$USER/C8B063EAB063DD86/cudnn-10.1-linux-x64-v7.6.5.32.tgz -t /home/$USER
+#tar -xvzf cudnn-10.1-linux-x64-v7.6.5.32.tgz
+#sudo cp /media/$USER/C8B063EAB063DD86/fixerror.sh -t /home/$USER
+#./fixerror.sh
 
 sudo apt-get update 
 sudo apt install mailutils -y 
